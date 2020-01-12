3eeeef1570f3414bb1a09857449d688bfa3a4608
christianarredondo <christianarredondo@utexas.edu>
2020-01-12 09:20:18 -0600

    linked-list demo

---
+++ interview-practice/linked-lists/linked-list.js
@@ -0,0 +1,126 @@
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = {
+      value,
+      next: null
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  };
+
+  prepend(value) {
+    const newNode = {
+      value,
+      next: this.head
+    };
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  insert(insertIndex, value) {
+    if (insertIndex > this.length - 1) {
+      throw new Error('Index out of range of list');
+    }
+
+    if (insertIndex === 0) {
+      this.prepend(value);
+      return this;
+    }
+
+    if (insertIndex === this.length - 1) {
+      this.append(value);
+      return this;
+    }
+
+    const nodeBeforeInsert = this._getNodeAtIndex(insertIndex - 1);
+    const newNode = {
+      value,
+      next: nodeBeforeInsert.next
+    };
+    nodeBeforeInsert.next = newNode;
+    this.length++;
+    return this;
+  }
+
+  delete(deleteIndex) {
+    if (deleteIndex > this.length - 1) {
+      throw new Error('Index out of range');
+    }
+
+    if (deleteIndex === 0) {
+      this.head = this.head.next;
+      this.length--;
+      return this;
+    }
+
+    const nodeBeforeDelete = this._getNodeAtIndex(deleteIndex - 1);
+    const nodeToDelete = nodeBeforeDelete.next;
+    const nodeAfterDelete = nodeToDelete.next;
+    nodeBeforeDelete.next = nodeAfterDelete;
+
+    this.length--;
+    return this;
+  }
+
+  print() {
+    let current = this.head;
+    let string = '';
+    while (current) {
+      if (!string) {
+        string += `${current.value}`;
+      } else {
+        string += ` --> ${current.value}`;
+      }
+      current = current.next;
+    }
+    console.log(`\nList: ${string}`, `\nLength: ${this.length}`);
+    return this;
+  }
+
+  _getNodeAtIndex(index) {
+    if (index === 0) {
+      return this.head;
+    }
+
+    if (index === this.length - 1) {
+      return this.tail;
+    }
+
+    let ref = this.head;
+    let currentIndex = 0;
+    while (currentIndex < index - 1) {
+      ref = ref.next;
+      currentIndex++;
+    }
+
+    return ref;
+  }
+}
+
+const list = new LinkedList(10);
+list
+  .append(5)
+  .append(16)
+  .prepend(1)
+  .append(100)
+  .insert(1, 2)
+  .print()
+  .delete(1)
+  .print()
+  .delete(4)
+  .print()
+  .delete(0)
+  .print();
