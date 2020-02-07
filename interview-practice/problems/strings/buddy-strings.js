a79c5894458140417a0cb8af9fd40a3a54c33a9c
christianarredondo <christianarredondo@utexas.edu>
2020-02-07 05:04:47 -0600

    add buddy-strings description

--- interview-practice/problems/strings/buddy-strings.js
+++ interview-practice/problems/strings/buddy-strings.js
@@ -1,4 +1,8 @@
-
+/**
+ * Given two strings A and B of lowercase letters,
+ * return true if and only if we can swap two letters
+ * in A so that the result equals B.
+ */
 const buddyStrings = (A = '', B = '') => {
   if (A.length !== B.length) return false;
   
