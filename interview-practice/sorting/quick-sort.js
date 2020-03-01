7e3c46ef2732a7f1ae0ce6cedba6907e995bc019
christianarredondo <christianarredondo@utexas.edu>
2020-03-01 11:28:43 -0600

    in-place quick-sort

--- interview-practice/sorting/quick-sort.js
+++ interview-practice/sorting/quick-sort.js
@@ -20,5 +20,31 @@ const quickSort = nums => {
   ];
 };
 
+const quickSortInPlace = nums => {
+  if (nums.length <= 1) return nums;
+  if (nums.length === 2) return nums[0] <= nums[1] ? nums : [nums[1], nums[0]];
+
+  let pivotIndex = nums.length - 1;
+  const initPivotNum = nums[pivotIndex];
+
+  let iter = 0;
+  while (iter < pivotIndex) {
+    const curr = nums[iter];
+    if (curr > initPivotNum) {
+      nums.push(nums.splice(iter, 1)[0]);
+      pivotIndex--;
+      continue;
+    }
+    iter++;
+  }
+
+  return [
+    ...quickSortInPlace(nums.slice(0, pivotIndex)),
+    initPivotNum,
+    ...quickSortInPlace(nums.slice(pivotIndex + 1))
+  ];
+};
+
 console.log('\n');
 console.log(quickSort([3, 1, 5, 6, -1, 2, 9, 2]));
+console.log(quickSortInPlace([3, 1, 5, 6, -1, 2, 9, 2]));
