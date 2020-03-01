d4183b4b004801456926d1219eace969db2a31a8
christianarredondo <christianarredondo@utexas.edu>
2020-03-01 10:12:07 -0600

    quick-sort

---
+++ interview-practice/sorting/quick-sort.js
@@ -0,0 +1,24 @@
+
+const quickSort = nums => {
+  if (nums.length <= 1) return nums;
+  if (nums.length === 2) return nums[0] <= nums[1] ? nums : [nums[1], nums[0]];
+
+  let pivotIndex = nums.length - 1;
+  const left = [];
+  const right = [];
+  const same = [nums[pivotIndex]];
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] < nums[pivotIndex]) left.push(nums[i]);
+    else if (nums[i] > nums[pivotIndex]) right.push(nums[i]);
+    else same.push(nums[i]);
+  }
+
+  return [
+    ...quickSort(left),
+    ...same,
+    ...quickSort(right)
+  ];
+};
+
+console.log('\n');
+console.log(quickSort([3, 1, 5, 6, -1, 2, 9, 2]));
